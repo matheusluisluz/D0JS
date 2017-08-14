0a4fbc296452c72086bd788f83a5d503e81f44dc
logonrm <logonrm@fiap.com.br>
2017-08-14 19:33:34 -0300

    d0

---
+++ js/principal.js
@@ -0,0 +1,9 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+
+var paciente = document.querySelector("#primeiro-paciente");
+var peso = paciente.querySelector(".info-peso");
+
+console.log(peso);
+console.log(paciente);
