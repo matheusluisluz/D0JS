f694d4b02f97ac725539205d4c9df1d386af36d6
MatheusLuis <suetham_luis@hotmail.com>
2017-08-19 23:26:27 -0300

    Animando a remocao do pacinte

---
+++ js/remover-paciente.js
@@ -0,0 +1,21 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+console.log(pacientes);
+
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick",function(event) {
+    event.target.parentNode.classList.add("fadeOut");
+    
+    setTimeout(function(){
+        event.target.parentNode.remove();
+    },500);
+    
+});
+
+/*pacientes.forEach(function(paciente) {
+     paciente.addEventListener("dblclick",function() {
+         console.log("Fui clicado com um duplo clique");
+         this.remove();
+     });   
+});*/
